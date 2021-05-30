99eeff1d3d7dcfa963c449931843ba1e2af82013
Drake Scott <drakescott999@hotmail.com>
2021-05-29 21:02:22 -0400

    Initial commit

---
+++ src/main/java/org/example/area.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class area {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the length of the room in feet? ");
+        double length = input.nextDouble();
+        System.out.print("What is the width of the room in feet? ");
+        double width = input.nextDouble();
+        System.out.println("You entered dimensions of " + length + " feet by " + width + " feet.\nThe area is");
+
+        double conversion = 0.09290304;
+        double areaFeet = length * width;
+        double areaMeters = areaFeet * conversion;
+
+        System.out.println(areaFeet + " square feet");
+        System.out.println(areaMeters + " square meters");
+    }
+}
